ee14180591b86c485b9e32c755e57dd1beb4753c
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2024-10-19 22:42:38 -0300

    feat: horizontal control

---
+++ Assets/Scripts/InputControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class InputControl : MonoBehaviour
+    {
+        public float speed;
+
+        public float Vertical
+        {
+            get => Input.GetAxis("Vertical");
+        }
+
+        public float Horizontal
+        {
+            get => Input.GetAxis("Horizontal");
+        }
+
+        public Vector3 Movement
+        {
+            get => new Vector3(Horizontal, 0, Vertical) * speed;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,26 +5,25 @@ using Assets.Scripts;
 
 public class PlayerController : Powerup
 {
+    public float speed = 5.0f;
+    private InputControl control;
+
     private Rigidbody rb;
     private GameObject focalPoint;
-    private float VerticalInput
-    {
-        get => Input.GetAxis("Vertical");
-    }
 
-    public float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        control = gameObject.AddComponent<InputControl>();
+        control.speed = speed;
         rb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
-    new void Update()
+    private void FixedUpdate()
     {
-        base.Update();
-        rb.AddForce(VerticalInput * speed * focalPoint.transform.forward);
+        rb.AddForce(control.Movement);
     }
 }
